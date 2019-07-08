cfa0ae6ce70d2a5ea6d488606a2397c4bc07eeb6
Denis <disa_shatura@mail.ru>
2019-07-08 22:07:16 +0300

    fix bugs in device orientation

---
+++ Inc/myMsg.h
@@ -0,0 +1,8 @@
+#ifndef __myMSG_H
+#define __myMSG_H
+
+struct myMsg{
+	char* msg;
+	int size;
+};
+#endif
---
+++ Src/switchingControl.c
@@ -0,0 +1,34 @@
+#include "switchingControl.h"
+
+#include "main.h"
+#include "Wheelchair.h"
+#include "Reorientation.h"
+
+#define IN 1
+#define OUT 0
+
+extern int8_t switchMode;
+
+
+void checkStopStartCondition(DAC_HandleTypeDef* hdac,int16_t sensLEFT, struct Finish const *  finish){
+
+	static uint32_t tickBut=0;
+	static uint64_t tickRunDevice = 0;
+	static uint8_t state = OUT;
+	if (finish->gyroData.y < -sensLEFT && HAL_GetTick() - tickBut >= 250){
+		tickBut = HAL_GetTick();
+		tickRunDevice = HAL_GetTick();
+		if(switchMode == RemoteON){
+			stopWheelchair(hdac);
+			state = IN;
+		}else{
+			switchMode = ~switchMode;
+		}
+	}
+	if((HAL_GetTick() - tickRunDevice) >= 3000 && state == IN){
+		state = OUT;
+		setReorientationParam(&finish->accelData);
+		switchMode = ~switchMode;
+	}
+
+}
